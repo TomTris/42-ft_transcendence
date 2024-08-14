8a864d66e757491368a716b8cb4426936489dfdc
Robin-Marq <129085102+Robin-Marq@users.noreply.github.com>
2024-08-14 22:57:57 +0200

    #Robins commit, check out the first design if it is ok for you. We can use other collors later, but is this in the first step the way you think?! Think yes, right?! Try it out and start with the index.html button !

---
+++ ft_transcendence/templates/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	// when document loaded:
+
+	$("#Playbutton1P").on("click",function() {
+
+	});
+
+	$("#Playbutton1P").on("mouseenter",function() {
+
+	});
+
+	$("#Playbutton2P").on("click",function() {
+
+	});
+
+	$("#Playbutton2P").on("mouseenter",function() {
+
+	});
+
+	$("#div4").on("click",function() {
+
+	});
+
+	$("#PlaybuttonMP").on("click",function() {
+
+	});
+
+	$("#PlaybuttonMP").on("mouseenter",function() {
+
+	});
+
+	$("#ta6").on("mouseenter",function() {
+
+	});
+
+});
