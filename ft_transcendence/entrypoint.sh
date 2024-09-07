b059deb56502217e36005f71871f2d24d86f5fe0
TomTris <quoctrido.jb@gmail.com>
2024-09-07 12:25:18 +0200

    improve script

--- ft_transcendence/entrypoint.sh
+++ ft_transcendence/entrypoint.sh
@@ -21,4 +21,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Start the Django development server
-python manage.py runserver 0.0.0.0:8000
+python manage.py users/send_email && python manage.py runserver 0.0.0.0:8000
--- ngrok_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -e ngrok_running ]; then
-docker run --net=host -it -e NGROK_AUTHTOKEN=$(cat ./.env | grep "NGROK_AUTHTOKEN=" | cut -c17- | sed "s/^['\"]//;s/['\"]$//") \
-        ngrok/ngrok:latest http --domain=$(cat ./.env | grep "DOMAIN_NAME=" | cut -c14- | sed "s/^['\"]//;s/['\"]$//") 443 2>/dev/null | echo ""
-touch ngrok_running
-fi
