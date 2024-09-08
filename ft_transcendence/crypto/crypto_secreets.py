928fde800e39d0653dea8257f72e38cc4f944aba
TomTris <quoctrido.jb@gmail.com>
2024-09-08 17:57:16 +0200

    Last change!

--- ft_transcendence/crypto/crypto_secreets.py
+++ ft_transcendence/crypto/crypto_secreets.py
@@ -2,14 +2,10 @@ import os
 import json
 from django.conf import settings
 
-# private_key=settings.PRIVATE_KEY
-# infura_url=settings.INFURA_URL
-# contract_address=settings.CONTRACT_ADDRESS
-# deployer_account=settings.DEPLOYER_ACCOUNT
-private_key='c051c92059091ba347242e96aca866a53cc31619be8a36d446c5dc0fe6551341'
-infura_url='https://holesky.infura.io/v3/29c81c46168644f28a5b97385d282e11'
-contract_address='0x03c40A2BB92a10aFbD507a1184ff0D834b246d93'
-deployer_account='0xd2acb8D6BB9C833D1aC4d82746224AEb6bc403f8'
+private_key=settings.PRIVATE_KEY
+infura_url=settings.INFURA_URL
+contract_address=settings.CONTRACT_ADDRESS
+deployer_account=settings.DEPLOYER_ACCOUNT
 
 with open('./crypto/TournamentContractABI.json') as file:
     contract_abi = json.load(file) 
